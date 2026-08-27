649ac343022ba840d0a71b8f7981dba5dc01882e
pasupathi-raj <pasupathi.raj@emertxe.com>
2026-08-27 15:42:08 +0530

    Updated syntax

--- classes/crm_fields_helper.php
+++ classes/crm_fields_helper.php
@@ -102,7 +102,6 @@ class crm_fields_helper {
         }
         return $keys;
     }
-<<<<<<< HEAD
 
     /**
      * Strip restricted CRM fields from a data array.
@@ -256,6 +255,4 @@ class crm_fields_helper {
             ];
         }, self::normalise_mentor_field_groups($groups));
     }
-=======
->>>>>>> origin/main
 }
